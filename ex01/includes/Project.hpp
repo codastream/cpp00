945979501e59ba3d8686bb5f806ebbe449da67dc
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-04-27 15:33:48 +0200

    [feat] instanciation ex01

---
+++ ex01/includes/Project.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpetit <fpetit@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 15:15:19 by fpetit            #+#    #+#             */
+/*   Updated: 2025/04/27 15:31:47 by fpetit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONE_BOOK_HPP
+# define PHONE_BOOK_HPP
+
+# include <iostream>
+# include "Contact.hpp"
+# include "PhoneBook.hpp"
+
+#endif
